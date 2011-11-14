0fb0a75f4b35647854290c1dd5141b068adf4558
Scott B. Bradley <scott@codeslick.com>
2011-11-14 17:30:19 -0600

    fixed license info

--- multimail/__init__.py
+++ multimail/__init__.py
@@ -3,8 +3,8 @@
 .. moduleauthor:: Scott B. Bradley <scott@scott2b.com>
 """
 __version__ = '0.1.0'
-__license__ = ''
+__license__ = 'The MIT License (MIT)'
 
-__author__ = 'scott2b'
+__author__ = 'scott2b <Scott B. Bradley>'
 __email__ = 'scott@scott2b.com'
 __url__ = 'http://multimail.scott2b.com'
