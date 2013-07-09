5f7b73b66363679a6d1f9455f7174b966deeae9b
Scott Bradley <scott.b.bradley@gmail.com>
2013-07-08 20:58:50 -0500

    Second attempt to fix license

--- multimail/__init__.py
+++ multimail/__init__.py
@@ -3,7 +3,7 @@
 .. moduleauthor:: Scott B. Bradley <scott@scott2b.com>
 """
 __version__ = '0.1.1'
-__license__ = 'License :: OSI Approved :: MIT License'
+__license__ = 'OSI Approved :: MIT License'
 
 __author__ = 'scott2b <Scott B. Bradley>'
 __email__ = 'scott@scott2b.com'
