18146cd4fac2c7c85759f794f4d42e9eb59e434a
Scott Bradley <scott.b.bradley@gmail.com>
2013-07-10 13:13:01 -0500

    Fixes include_recursive manifest info; bumps version to 0.1.2

--- multimail/__init__.py
+++ multimail/__init__.py
@@ -2,7 +2,7 @@
 
 .. moduleauthor:: Scott B. Bradley <scott@scott2b.com>
 """
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __license__ = 'OSI Approved :: MIT License'
 
 __author__ = 'scott2b <Scott B. Bradley>'
