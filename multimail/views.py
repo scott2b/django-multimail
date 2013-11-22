e4718e5d6f371f3fd9903d6333cbb5791681a1d7
Daniel Barreto <dbarreto@talpor.com>
2013-11-22 15:46:16 -0430

    Aaand another typo.

--- multimail/views.py
+++ multimail/views.py
@@ -69,7 +69,7 @@ def set_as_primary(request, email_pk):
     """Set the requested email address as the primary. Can only be
     requested by the owner of the email address."""
     email = get_object_or_404(EmailAddress, pk=email_pk)
-    if email.is_verified():
+    if not email.is_verified():
         messages.error(request, 'Email %s needs to be verified first.' % email)
     if email.user != request.user:
         messages.error(request, 'Invalid request.')
